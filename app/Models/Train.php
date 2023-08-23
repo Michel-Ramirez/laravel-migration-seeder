8a9318e401fc0e9314e1c59bde021dc9aae9d312
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-08-23 16:54:48 +0200

    add methods

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
+
+    public function isTimely()
+    {
+
+        // if ($this->is_deleted) {
+        //     return $this->is_timely = '';
+        // } else {
+
+        // }
+        return $this->is_timely ? 'IN RITARDO' : 'IN ORARIO';
+    }
+
+    public function isDeleted()
+    {
+        return $this->is_deleted ? 'CANCELLATO' : 'IN TRANSITO';
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,56 @@
 @section('title', 'Home')
 
 @section('main')
-    <h1>Benvenuto</h1>
+    <div class="wrapper">
+        <h1>Treni in trasito</h1>
+
+        <div class="table">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h5>NUMERO TRENO</h5>
+                        @foreach ($trains as $train)
+                            <p>{{ $train->train_id }}</p>
+                        @endforeach
+                    </div>
+                    <div class="col">
+                        <h5>PARTENZE</h5>
+                        @foreach ($trains as $train)
+                            <p>{{ $train->departure_station }}</p>
+                        @endforeach
+                    </div>
+                    <div class="col">
+                        <h5>ARRIVI</h5>
+                        @foreach ($trains as $train)
+                            <p>{{ $train->departure_station }}</p>
+                        @endforeach
+                    </div>
+                    <div class="col">
+                        <h5>PARTENZA</h5>
+                        @foreach ($trains as $train)
+                            <p>{{ $train->departure_time }}</p>
+                        @endforeach
+                    </div>
+                    <div class="col">
+                        <h5>ARRIVO</h5>
+                        @foreach ($trains as $train)
+                            <p>{{ $train->arrival_time }}</p>
+                        @endforeach
+                    </div>
+                    <div class="col">
+                        <h5>RITARDO</h5>
+                        @foreach ($trains as $train)
+                            <p>{{ $train->isTimely() }}</p>
+                        @endforeach
+                    </div>
+                    <div class="col">
+                        <h5>CANCELLATI</h5>
+                        @foreach ($trains as $train)
+                            <p class="{{ $train->isDeleted() }}">{{ $train->isDeleted() }}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
